4c8e7a266bb8302b3ed55ce44b927e1551c97881
Huzaifa <HuzaifaIslamRakib@gmail.com>
2024-12-21 19:57:47 +0600

    made auth for sign up in with email pass and only google

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -2,12 +2,35 @@ import { useEffect, useState } from "react";
 import { AuthContext } from "../Contexts/Contexts";
 import PropTypes from "prop-types";
 import auth from "../Firebase/Firebase_config";
-import { onAuthStateChanged } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
   // state
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  //   sign up with gmail and pass
+  const singUpWithEmail = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //   sign in with gmail and pass
+  const signInWithEmail = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  //sign in or up with gmail
+  const signWithGmail = () => {
+    return signInWithPopup(auth, provider);
+  };
 
   // observer
   useEffect(() => {
@@ -23,6 +46,9 @@ const AuthProvider = ({ children }) => {
     setUser,
     loading,
     setLoading,
+    singUpWithEmail,
+    signInWithEmail,
+    signWithGmail,
   };
   return <AuthContext.Provider value={info}>{children}</AuthContext.Provider>;
 };
